b45da837369092928fa58d3c7a88bc6bc696a1fe
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2022-04-21 08:25:55 -0300

    Created .travis.yml File

--- src/main/java/com/rodrigopeleias/bookstoremanager/controller/BookController.java
+++ src/main/java/com/rodrigopeleias/bookstoremanager/controller/BookController.java
@@ -4,19 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-
 @RestController
 @RequestMapping(value = "/api/v1/books")
 public class BookController {
 
 	@GetMapping
-	@Operation(description = "Return a hello world example")
-	@ApiResponses(value = {
-			@ApiResponse(responseCode = "200", description = "Success method returned")
-	})
 	public String hello() {
 		return "Hello Bookstore Manager";
 	}
